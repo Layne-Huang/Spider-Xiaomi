d5a8a703fa131363de6d54afff5f68e66bb62abd
Layne_Huang <1847707192@qq.com>
2019-04-09 12:12:05 +0800

    Add files via upload

--- Xiaomi.py
+++ Xiaomi.py
@@ -24,7 +24,7 @@ options = webdriver.ChromeOptions()
 prefs = {
     'profile.default_content_setting_values': {
         'images': 2
-    }
+    }#将浏览器设置为不显示图片
 }
 options.add_experimental_option('prefs', prefs)
 class XiaomiComment():
@@ -32,10 +32,12 @@ class XiaomiComment():
         self.url = 'http://list.mi.com/'
         self.row = 1
         self.col = 0
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(chrome_options=options)
+        self.driver2 = webdriver.Chrome(chrome_options=options)
         self.workbook = xlwt.Workbook(encoding='utf-8')
         self.sheet = list
 
+    #获得代理ip
     def get_proxy(self):
         PROXY_POOL_URL = 'http://localhost:5000/get'
 
@@ -51,8 +53,9 @@ class XiaomiComment():
 
 # driver.get(url)
 # driver.maximize_window()
-    def anti_anti_spider(self):
 
+    #一种反反爬虫措施
+    def anti_anti_spider(self):
         self.driver.delete_all_cookies()
         js1= '''Object.defineProperties(navigator,{ webdriver:{ get: () => false } }) '''
         js2= '''window.navigator.chrome = { runtime: {},  }; '''
@@ -63,63 +66,91 @@ class XiaomiComment():
         self.driver.execute_script(js3)
         self.driver.execute_script(js4)
 
-
+    #进入小米商品列表，获得关于小米手机的信息
     def getList(self):
         self.driver.get(self.url)
         self.driver.maximize_window()
         phones = self.driver.find_elements_by_css_selector("a[class='category-list-title']")
+        global sheet
+        global i
+        sheet = []
         i = 0
         for phone in phones:
+            if "小米移动" in phone.text:
+                return phones
             if "小米" in phone.text:
+                print(i)
                 now_handle = self.driver.current_window_handle
-                phone.click()
-                self.driver.find_element_by_css_selector("#J_headNav > div > div > div.right > a:nth-child(7)").click()
+                self.driver2.get(phone.get_attribute('href'))
+                time.sleep(2)
+                comment_button = self.driver2.find_element_by_css_selector("#J_headNav > div > div > div.right > a:nth-child(7)")
+                if "评价" not in comment_button.text:
+                    comment_button = self.driver2.find_element_by_xpath('//a[text()="用户评价"]')
+                comment_button.click()
+                time.sleep(2)
                 self.next_page()
-                global sheet
-                sheet = list
-                sheet[i] = self.workbook.add_sheet(phone.text)  # 新建sheet
+                print(phone.text)
+                sheet.append(self.workbook.add_sheet(phone.text))  # 新建sheet
+                print("数组长度为:",len(sheet))
                 sheet[i].write(0, 0, "评论")
                 sheet[i].write(0, 1, "时间")
                 self.get_detail()
-
-
                 i+=1
+
         return phones
 
 
 # driver.switch_to.window(driver.window_handles[0])
-    def get_detail(self,phones):
-        i = 0
-        comments = self.driver.find_elements_by_css_selector('div[class="comment-txt"]')
+    #获得具体的一个小米手机的评论
+    def get_detail(self):
+        amount = 0
+        comments = self.driver2.find_elements_by_css_selector('div[class="comment-txt"]')
         for comment in comments:
             comment_text = comment.text
-            print(comment_text)
+            # print(comment_text)
             sheet[i].write(self.row, self.col, comment_text)
+            amount+=1
             self.row += 1
-        dates = self.driver.find_elements_by_css_selector('p[class="time"]')
+        dates = self.driver2.find_elements_by_css_selector('p[class="time"]')
         self.col+=1
         self.row = 1
         for date in dates:
             date_text = date.text
-            print(date_text)
+            # print(date_text)
             sheet[i].write(self.row,self.col,date_text)
             self.row += 1
         self.row = 1
         self.col = 0
+        print("有：",amount,"条评论")
+
+
 
     def next_page(self):
-        learn_more = self.driver.find_elements_by_css_selector(
+        count = 0
+        flag = True
+        learn_more = self.driver2.find_elements_by_css_selector(
             'body > div.m-comment-wrap.h-comment-wrap > div.container.J_commentWrap > div.row > div.span13.h-comment-main.m-comment-main.J_commentCon > div.m-comment-box.J_commentList > div > a')
-        while(len(learn_more)!=0):
-            for x in learn_more:
-                x.click()
+        while(len(learn_more)!=0 and flag):
+            try:
+                for x in learn_more:
+                    x.click()
+                    count+=1
+                    print(count)
+                    if count== 80:
+                        print("点了80次了")
+                        flag = False
+                        break
+            except:
+                print("Pages have already loaded!!")
+                break
 
     def run(self):
 
         self.anti_anti_spider()
         self.get_proxy()
         self.getList()
-        self.workbook.save(r'D:\University_study\junior_second half\信息管理课程设计\data\小米6淘宝.xlsx')
+        self.workbook.save(r'D:\University_study\junior_second half\信息管理课程设计\data\小米商城.xlsx')
+        print("Files are succssfuly saved!")
 
 if __name__ == '__main__':
     comment = XiaomiComment()
